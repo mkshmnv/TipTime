096347287a5b66430cd7a3506c54d8e1044a73e5
Mykola <nickkosh@gmail.com>
2023-06-05 10:46:43 +0300

    binding

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -12,10 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener{ calculateTip() }
     }
 
     private fun calculateTip() {
-        // Get the decimal value from the cost of service text field
+
         val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
     }
